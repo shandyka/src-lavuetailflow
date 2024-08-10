f41bb08c151130128b75d8c0afb291abd517310b
Shandyka PW <32282713+shandyka@users.noreply.github.com>
2024-08-10 17:02:26 +0700

    Add files via upload

---
+++ hero-style1.blade.php
@@ -0,0 +1,23 @@
+<!-- awal hero -->
+<section class="bg-white dark:bg-gray-900">
+    <div class="animation-background"></div>
+</div>
+  <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12" style="margin-top: 5vh;">
+      <div class="mr-auto place-self-center lg:col-span-7" style="margin-top: 5vh;">
+          <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Percetakan Buku Berkualitas Tinggi</h1>
+          <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Java Book Center menawarkan layanan percetakan buku dengan harga kompetitif dan kualitas terbaik. Temukan solusi percetakan yang efisien dan terpercaya untuk kebutuhan Anda.</p>
+          <a href="#shop" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+              Pesan Sekarang
+              <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+          </a>
+          <a href="#contact" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+              Hubungi Kami
+          </a> 
+      </div>
+      <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
+          <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhBLdK7abBYc90khbiGj1NoETJ1uVbZm1pr0TDru_nMsoaCb-4nu_-V-4ZejXXDOFJC3zTJr0rdi0f-TEaNMKpRJGs1yrNmFQDyx0RbO8sIVQyMI682cAYOL-5gxAnL_oWEG1nw4XfQpYcsie6_o0RdMgRoNahKqXP2mSgcAFaJLKMTtVY9d1J2bLH6NEHR/s1600/Blocknotemock2.png" alt="mockup">
+      </div>                
+  </div>
+  
+</section>
+<!-- akhir hero -->
